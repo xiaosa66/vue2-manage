6d4e6c1febd93fea20e0bb739bdb75e851689681
Xiaosa Liu <liu.changsen@qq.com>
2019-03-29 20:59:47 +0800

    打包项目引起的变化

--- config/env.js
+++ config/env.js
@@ -18,8 +18,8 @@ if (process.env.NODE_ENV == 'development') {
     imgBaseUrl = '/img/';
 
 }else if(process.env.NODE_ENV == 'production'){
-    baseUrl = '//elm.cangdu.org';
-    imgBaseUrl = '//elm.cangdu.org/img/';
+    baseUrl = 'http://localhost:3000';
+    imgBaseUrl = '/img/';
 }
 
 export {
